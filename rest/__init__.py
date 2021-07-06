ff21ff668ab35dda1306915ff71a44dbf75f6cfb
Charitha Ratnayake <charitha.r@eyepax.com>
2021-07-06 21:10:53 +0530

    Implemented
    - login and register APIs

--- rest/__init__.py
+++ rest/__init__.py
@@ -5,11 +5,15 @@ import blockchain
 from datetime import datetime
 import model as model
 import json
+import rest.response
+from rest.response import Response
+import hashlib
 
 from blockchain.__init__ import Block, Blockchain as bc
 
 app = flask.Flask(__name__)
 db_cursor = db.db_cursor
+connection = db
 
 
 @app.route('/getEssentials', methods=['POST'])
@@ -22,17 +26,15 @@ def getEssentials():
 @app.route('/register', methods=['POST'])
 def register():
     msg_received = flask.request.get_json()
-    msg_subject = msg_received["subject"]
-
-    return register(msg_received)
+    rsp = register(msg_received)
+    return json.dumps(rsp.__dict__)
 
 
 @app.route('/login', methods=['POST'])
 def login():
     msg_received = flask.request.get_json()
-    msg_subject = msg_received["subject"]
-
-    return login(msg_received)
+    rsp = login(msg_received)
+    return json.dumps(rsp.__dict__)
 
 
 @app.route('/chain', methods=['GET'])
@@ -45,7 +47,6 @@ def get_chain():
 
 
 def register(msg_received):
-    uid = msg_received["uid"]
     user_type = msg_received["user_type"]
     full_name = msg_received["full_name"]
     email = msg_received["email"]
@@ -54,39 +55,65 @@ def register(msg_received):
     birth_day = msg_received["birth_day"]
     address = msg_received["address"]
     gender = msg_received["gender"]
+    is_active = 1
+    created_at = 1
+    updated_at = 1
 
-    select_query = "SELECT * FROM users where email = " + "'" + email + "'"
+    data_user = (email, user_type, full_name, email, password, phone_number, birth_day, address, gender, is_active,
+                 created_at, updated_at)
+
+    select_query = "SELECT * FROM user where email = " + "'" + email + "'"
     db_cursor.execute(select_query)
     records = db_cursor.fetchall()
     if len(records) != 0:
-        return "Another user used the username. Please chose another username."
+        p1 = Response(1, "Your email is already registered with the system.", None)
+        return p1
 
-    insert_query = "INSERT INTO users (first_name, last_name, username, password) VALUES (%s, %s, %s, MD5(%s))"
-    insert_query = "INSERT INTO `user` (`id`, `uid`, `user_type`, `full_name`, `email`, `password`, `phone_number`, " \
-                   "`birth_day`, `address`, `gender`, `is_active`, `created_at`, `updated_at`) " \
-                   "VALUES (NULL, MD5(%s), %s, %s, %s, MD5(password), %s, %s, %s, %s, '1', '" + datetime.now().timestamp() + "', '" + datetime.now().timestamp() + "'))"
-    insert_values = (uid, user_type, full_name, email, password, phone_number, birth_day, address, gender)
+    insert_query = '''INSERT INTO `user` (`id`, `uid`, `user_type`, `full_name`, `email`, `password`, `phone_number`, 
+                    `birth_day`, `address`, `gender`, `is_active`, `created_at`, `updated_at`) 
+                   VALUES (NULL, MD5(%s), %s, %s, %s, MD5(%s), %s, %s, %s, %s, %s, %s, %s)'''
+
+    print(insert_query)
+    print(data_user)
+
     try:
-        db_cursor.execute(insert_query, insert_values)
-        db_cursor.commit()
-        return "success"
+        db_cursor.execute(insert_query, data_user)
+        connection.db.commit()
+
+        select_query = "SELECT * FROM user where email = " + "'" + email + "' and password = " + "MD5('" + password + "')"
+        db_cursor.execute(select_query)
+        row_headers = [x[0] for x in db_cursor.description]
+        records = db_cursor.fetchall()
+        json_data = []
+        for result in records:
+            json_data.append(dict(zip(row_headers, result)))
+
+        p1 = Response(0, "Success.", json_data[0])
+        return p1
     except Exception as e:
         print("Error while inserting the new record :", repr(e))
-        return "failure"
+        p1 = Response(1, "Your email or phone number is already registered with the system.", None)
+        return p1
 
 
 def login(msg_received):
     email = msg_received["email"]
     password = msg_received["password"]
 
-    select_query = "SELECT first_name, last_name FROM users where username = " + "'" + email + "' and password = " + "MD5('" + password + "')"
+    select_query = "SELECT * FROM user where email = " + "'" + email + "' and password = " + "MD5('" + password + "')"
     db_cursor.execute(select_query)
+    row_headers = [x[0] for x in db_cursor.description]
     records = db_cursor.fetchall()
+    json_data = []
+    for result in records:
+        json_data.append(dict(zip(row_headers, result)))
 
     if len(records) == 0:
-        return "failure"
+        p1 = Response(1, "User not found.", None)
+        return p1
     else:
-        return "success"
+        p1 = Response(0, "Success", json_data[0])
+        return p1
 
 
 def getEssentials(msg_received):
